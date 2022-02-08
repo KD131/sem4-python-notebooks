83397aced6b7bf357a56e7e4a3f0406611e2a3af
KD131 <cph-kd131@cphbusiness.dk>
2022-02-08 14:31:56 +0100

    import test moved to sibling folders

--- week2_modules/module2.py
+++
@@ -1,4 +0,0 @@
-import module1 as mod
-
-if __name__ == "__main__":
-    mod.test()
---
+++ week2_modules/package2/module2.py
@@ -0,0 +1,8 @@
+import week2_modules.package1.module1 as mod
+# from week2_modules.package1 import module1 as mod
+
+if __name__ == "__main__":
+    mod.test()
+
+# cd in my_notebooks/
+# python -m week2_modules.package2.module2
