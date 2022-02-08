73f1b6d0a0abf9597a57639960591740b2966b8c
KD131 <cph-kd131@cphbusiness.dk>
2022-02-08 16:11:46 +0100

    cli download last bit of path as default

--- week2_modules/cli_download.py
+++ week2_modules/cli_download.py
@@ -1,9 +1,14 @@
 import argparse
 import modules.webget as webget
+import os.path as path
+# we import from the teacher's modules folder, so the current folder is notebooks/
 
 parser = argparse.ArgumentParser(description="Tool to download from URL")
 parser.add_argument("url", help="The URL path to download")
-parser.add_argument("--destination", "-d", default="default_file.dat", help="The destination file to save the download in")
+parser.add_argument("--destination", "-d", help="The destination file to save the download in")
 args = parser.parse_args()
+
+if not args.destination:
+    args.destination = path.basename(path.normpath(args.url))
 print("Downloading " + args.url + " into " + args.destination)
 webget.download(args.url, args.destination)
