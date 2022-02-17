ef6644b5c2b4e603cc2537ab6c2be71bf79e9ecd
KD131 <71063280+KD131@users.noreply.github.com>
2022-02-17 01:18:33 +0100

    print name, url, and avg. grade of students

--- week3/school_classes_ex.py
+++ week3/school_classes_ex.py
@@ -19,9 +19,12 @@ with open("students.csv") as file:
     reader = csv.reader(file)
     headers = next(reader)
     for row in reader:
-        course = Course(row[1], row[5], row[2], row[4], row[6])
+        course = Course(row[1], row[5], row[2], int(row[4]), int(row[6]))
         student = next((s for s in students if s.image_url == row[7]), None) #using image_url as id because it's the closest we have to it.
         if student:
             student.data_sheet.courses.append(course)
         else:
             students.append(Student(row[0], row[3], DataSheet([course]), row[7]))
+
+for s in students:
+    print(f"{s.name}, URL: {s.image_url}, avg. grade: {s.get_average_grade()}")
