a57813f1cf27279370e1e6765765b48b40583829
KD131 <cph-kd131@cphbusiness.dk>
2022-03-22 14:23:33 +0100

    week 8 baby ex

---
+++ modules/week8/baby_features.py
@@ -0,0 +1,30 @@
+import random as rnd
+
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def generate_babies(n):
+    babies = []
+    for i in range(n):
+        height = rnd.randint(45,80)
+        weight = rnd.randint(2500,13000)
+        age = rnd.randint(0,12) # months
+        babies.append((height, weight, age))
+
+    return babies
+
+def plot_babies(babies):
+    np_babies = np.array(babies)
+    x, y, z = np_babies[:,0], np_babies[:,1], np_babies[:,2]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(x, y, z, linewidth=0.2)
+
+    plt.show()
+
+babies = generate_babies(20)
+print(babies)
+plot_babies(babies)
