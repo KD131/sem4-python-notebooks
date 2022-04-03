9a2e087731b18b96d61c6526f14c4215877ec717
KD131 <71063280+KD131@users.noreply.github.com>
2022-04-03 18:50:04 +0200

    changes scrape_links to selenium and switches context to iframe if url is a notebook

--- modules/scrape_links.py
+++ modules/scrape_links.py
@@ -1,18 +1,33 @@
 import bs4
 import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 def scrape_links(url, *starts_with):
-    r = requests.get(url)
-    r.raise_for_status()
-    soup = bs4.BeautifulSoup(r.text, "html.parser")
-    links = [link + "\n" for link in soup.select("a")
+    options = Options()
+    options.headless = True
+    browser = webdriver.Firefox(options=options)
+    browser.get(url)
+    
+    browser.implicitly_wait(2)
+    if url.endswith(".ipynb"):
+        browser.switch_to.frame(0)
+
+    source = browser.page_source
+    
+    browser.close()
+    browser.quit()
+    
+    # r = requests.get(url)
+    # r.raise_for_status()
+
+    soup = bs4.BeautifulSoup(source, "html.parser")
+    links = [link.get("href") + "\n" for link in soup.select("a")
                 if link.get("href")
                 and link.get("href").startswith(starts_with)]
-    with open("debug", "w", encoding="utf-8") as f:
-        f.write(r.text)
     return links
 
 if __name__ == "__main__":
     links = scrape_links("https://github.com/Hartmannsolution/docker_notebooks/blob/master/notebooks/00%20Videos.ipynb", "https://youtu.be/", "https://youtube.com/")
-    with open("links", "w") as file:
+    with open("links.txt", "w") as file:
         file.writelines(links)
